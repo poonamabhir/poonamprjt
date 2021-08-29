444c09a4e55a577ff3fc9d2cf5244925910e5750
poonam <poonamkudale29@gmal.com>
2021-08-29 07:59:54 +0530

    new prjt

---
+++ src/test/java/mavenTutorial/MavenDemo/AppiumTest.java
@@ -0,0 +1,19 @@
+package mavenTutorial.MavenDemo;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void Nativeapp()
+	{
+		System.out.println("Nativeapp");
+		System.out.println("I am in Y- MavanDemo");
+	}
+	@Test
+public void MobileApp()
+{
+	System.out.println("Mobile App");
+	System.out.println("I am mobile in Y- MavanDemo");
+}
+}
---
+++ src/test/java/mavenTutorial/MavenDemo/RESTAPITest.java
@@ -0,0 +1,19 @@
+package mavenTutorial.MavenDemo;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void postJira()
+	{
+		System.out.println("Post Jira");
+	}
+	
+	@Test
+	public void deletTwitter()
+	{
+		System.out.println("Delete Twitter account");
+	
+	}
+}
